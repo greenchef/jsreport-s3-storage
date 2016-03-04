07177755725f0542c9fd80ba8436b968143414e6
Franklin M Gauer III <fgauer@greenchef.com>
2016-03-03 17:19:18 -0700

    Initial commit of main branch

---
+++ jsreport.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'name': 's3-rendered-output-storage',
+  'main': './lib/main.js',
+  'dependencies': [],
+  'skipInExeRender': true
+}
---
+++ testlocal.bat
@@ -0,0 +1,4 @@
+d:
+cd d:\stcroixcode\jsreport-s3-storage
+xcopy lib "d:\jsreport\jsreport\node_modules\jsreport\extension\s3-rendered-output-storage\lib"
+copy jsreport.config.js "d:\jsreport\jsreport\node_modules\jsreport\extension\s3-rendered-output-storage\jsreport.config.js"  
