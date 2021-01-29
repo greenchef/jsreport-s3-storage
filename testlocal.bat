4c03e9189a0ad966b700272dcf5fe44abb5202a0
tom-con <tconchie@greenchef.com>
2021-01-29 16:14:53 -0700

    [DVOP-153] remove access keys from apps

--- testlocal.bat
+++
@@ -1,4 +0,0 @@
-d:
-cd d:\stcroixcode\jsreport-s3-storage
-xcopy/Y lib "d:\jsreport\jsreport\node_modules\jsreport\extension\s3-rendered-output-storage\lib"
-copy jsreport.config.js "d:\jsreport\jsreport\node_modules\jsreport\extension\s3-rendered-output-storage\jsreport.config.js"
